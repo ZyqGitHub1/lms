e770f24943772c8cb644d52a62817c7b4d7f8cb6
ZhangYongqin <qqdhsy@126.com>
2016-06-24 00:08:55 +0800

    bookinfo datatables create

--- static/js/bookmamage.js
+++ static/js/bookmamage.js
@@ -22,92 +22,61 @@ var request = window.superagent;
                 var $table = $('#dataTables-example');
                 var _table = $table.dataTable($.extend(
         true,{},CONSTANT.DATA_TABLES.DEFAULT_OPTION, {
-        "data":booklist,
-        // "aoColumns": [
-        //     {"mDataProp": "book_id",},
-        //     {"mDataProp": "book_no"},
-        //     {"mDataProp": "book_name"},
-        //     {"mDataProp": "book_writer"},
-        //     {"mDataProp": "book_publish"},
-        //     {"mDataProp": "book_price"},
-        //     {"mDataProp": "book_date"},
-        //     {"mDataProp": "book_class"},
-        //     {"mDataProp": "book_main"},
-        //     {"mDataProp": "book_prim"},
-        //     {"mDataProp": "book_state"},
-        //     {"mDataProp": "book_rno"},
-        //     {"mDataProp": "book_id"},
-        // ],
+        "data":TESTDATA.BOOKMANAGE,
         columns: [
             {
-                className : "ellipsis", //文字过长时用省略号显示，CSS实现
-                data: "book_id",
-                render : CONSTANT.DATA_TABLES.RENDER.ELLIPSIS,//会显示省略号的列，需要用title属性实现划过时显示全部文本的效果
+                "class":          'details-control',
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": ''
             },
             {
-                className : "ellipsis",
+                data: "book_id",
+            },
+            {
                 data: "book_no",
-                render : CONSTANT.DATA_TABLES.RENDER.ELLIPSIS,
-                //固定列宽，但至少留下一个活动列不要固定宽度，让表格自行调整。不要将所有列都指定列宽，否则页面伸缩时所有列都会随之按比例伸缩。
-                //切记设置table样式为table-layout:fixed; 否则列宽不会强制为指定宽度，也不会出现省略号。
-                width : "80px"
             },
             {
                 data : "book_name",
-                width : "80px",
-            },
-            {
-                data : "book_writer",
-                width : "80px"
-            },
-            {
-                data : "book_publish",
-                width : "80px"
-            },
-            {
-                data : "book_price",
-                width : "80px"
-            },
-            {
-                data : "book_date",
-                width : "80px"
             },
             {
                 data : "book_class",
-                width : "80px"
-            },
-            {
-                data : "book_main",
-                width : "80px"
-            },
-            {
-                data : "book_prim",
-                width : "80px"
             },
             {
                 data : "book_state",
-                width : "80px"
             },
             {
                 data : "book_rno",
-                width : "80px"
             },
             {
                 className : "td-operation",
                 data: null,
                 defaultContent:"",
                 orderable : false,
-                width : "120px"
             }
         ],
         "createdRow": function ( row, data, index ) {
             //行渲染回调,在这里可以对该行dom元素进行任何操作
             //不使用render，改用jquery文档操作呈现单元格
-            var $btnEdit = $('<button type="button" class="btn btn-small btn-primary btn-edit">修改</button>');
-            var $btnDel = $('<button type="button" class="btn btn-small btn-danger btn-del">删除</button>');
-            $('td', row).eq(12).empty();
-            $('td', row).eq(12).append($btnEdit).append($btnDel);
+            var $btnEdit = $('<a>修改</a>');
+            var $btnDel = $('<a>删除</a>');
+            //$('td', row).eq(7).empty();
+            $('td', row).eq(7).append($btnEdit).append("|").append($btnDel);
         },
     })).api();
+        $('#dataTables-example tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = _table.row( tr );
+        if ( row.child.isShown() ) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            // Open this row
+            row.child( format(row.data()) ).show();
+            tr.addClass('shown');
+        }
+    });
             }
         });
